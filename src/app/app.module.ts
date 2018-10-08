e36e6d6b75b580519af2a67f8c73365a918c32bd
Oscar Jimenez <fox.oje11@gmail.com>
2018-10-07 23:48:27 -0600

    Avance del curso, componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FrutaComponent } from './fruta/fruta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrutaComponent
   ],
   imports: [
     BrowserModule
